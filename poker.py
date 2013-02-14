b4ee4778291ae5683b1f9fd0aac9bb45e936ec50
Brian Husa <mrfixit42@gmail.com>
2013-02-14 18:46:05 +0900

    - More work

--- poker.py
+++ poker.py
@@ -13,7 +13,18 @@ def test():
 	assert poker([fh]) == fh
 	assert poker([sf]) == sf
 	assert poker([sf] + 99*[fh]) == sf
+	assert hand_rank(sf) == (8, 10)
+	assert hand_rank(fk) == (7, 9, 7)
+	assert hand_rank(fh) == (6, 10, 7)
 	return "tests pass"
 
 def hand_rank(hand):
+	"""
+	Return a value indicating the ranking of a hand.
+	"""
+	ranks = card_ranks(hand)
+	if straight(ranks) and flush(hand):
+		return (8, max(ranks)) # 2 3 4 5 6 (8, 6) 6 7 8 9 T (8, 10)
+	elif kind(4, ranks):
+		return (7, kind(4, ranks), kind(1, ranks)) # 9 9 9 9 3 (7, 9, 3)
 	return None
